a2bf674bf37b64cd53690a2a59232ca8893ab979
Beeldkameraden <beeldkameraden@Mac-mini-van-Beeldkameraden.local>
2025-08-29 13:38:36 +0200

    Head meta

---
+++ src/app/head.js
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>Beeldkameraden</title>
+      <meta name="description" content="Videobedrijf uit Groningen" />
+      <link rel="icon" href="/bkfavicon.svg" />
+   
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { IBM_Plex_Sans, IBM_Plex_Mono } from "next/font/google";
 import "./globals.css";
-import Head from 'next/head';
+
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
@@ -21,11 +21,6 @@ const ibmPlexMono = IBM_Plex_Mono({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>Beeldkameraden</title>
-        <meta name="description" content="Videobedrijf uit Groningen" />
-        <link rel="icon" href="/bkfavicon.svg" />
-      </Head>
       <body className={`${ibmPlexSans.variable} ${ibmPlexMono.variable} antialiased`} >
         <Navbar/>
         {children}
